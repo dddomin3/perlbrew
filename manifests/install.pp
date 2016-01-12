e34bf9f14dd09577fbc636f16b4abd8e75a15b99
leigh schrandt <leigh@programmer.net>
2016-01-11 19:51:54 -0700

    fixed variable syntax (missing $ signs) now concatenating the curl proxy string using interpolation

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,27 +17,27 @@ class perlbrew::install {
     mode   => '0755',
   }
   
-  http_proxy_string = ''
-  curl_http_proxy_string = ''
+  $http_proxy_string = ''
+  $curl_http_proxy_string = ''
   #build <[protocol://][user:password@]proxyhost[:port]>, prepends -x for curl too
   if($perlbrew::http_proxy&&$perlbrew::http_proxy_url) {
     #TODO: ensure url is valid url?
     #append username and password to proxy string
     if($perlbrew::http_proxy_username) {
       if($perlbrew::http_proxy_password) {
-        http_proxy_string += $perlbrew::http_proxy_username +':'+$perlbrew::http_proxy_password + '@'
+        $http_proxy_string += "${perlbrew::http_proxy_username}:${perlbrew::http_proxy_password}@"
       }
-      else { http_proxy_string += $perlbrew::http_proxy_username + '@' }
+      else { $http_proxy_string += $perlbrew::http_proxy_username + '@' }
     }
-    http_proxy_string += $perlbrew::http_proxy_url
+    $http_proxy_string += $perlbrew::http_proxy_url
     #TODO: curl -x http://proxy_server:proxy_port --proxy-user username:password -L http://url might be better supported
-    if($perlbrew::http_proxy_port) { http_proxy_string += ':'+$perlbrew::http_proxy_port }
-    curl_http_proxy_string = ' -x '+http_proxy_string 
+    if($perlbrew::http_proxy_port) { $http_proxy_string += ':'+$perlbrew::http_proxy_port }
+    $curl_http_proxy_string = ' -x '+$http_proxy_string 
   }
   
   exec {'install_perlbrew':
     environment => 'PERLBREW_ROOT=/opt/perl5',
-    command     => '/usr/bin/curl'+curl_http_proxy_string+' -L http://install.perlbrew.pl | /bin/bash',
+    command     => '/usr/bin/curl'+$curl_http_proxy_string+' -L http://install.perlbrew.pl | /bin/bash',
     creates     => "${perlbrew::perlbrew_root}/bin/perlbrew",
     require     => Package['curl'],
   }
