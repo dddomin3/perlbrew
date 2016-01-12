9acedcfcc31c6912182600a76ccd60450648d859
leigh schrandt <leigh@programmer.net>
2016-01-11 23:31:03 -0700

    moved if-block out of param list into resource block

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,14 +14,14 @@ class perlbrew (
   $perlbrew_init_file = $perlbrew::params::perlbrew_init_file,
   $http_proxy         = $perlbrew::params::http_proxy,
   
+) inherits perlbrew::params {
+
+  # param validation
+  
   if $http_proxy {
     $curl_http_proxy_string = "-x ${perlbrew::http_proxy}"
   }
   
-) inherits perlbrew::params {
-
-  # param validation
-
   class { 'perlbrew::install': } ->
   class { 'perlbrew::config': }
   
